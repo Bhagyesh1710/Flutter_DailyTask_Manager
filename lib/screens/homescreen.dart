93c620f1cd9dca4fdac942b1fc459558359afdec
Bhagyesh <bhagyesh.imscit17@gmail.com>
2022-01-17 17:57:19 +0530

    Display Task Data

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -7,7 +7,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:flutter_dailytask/model/task.dart';
 
-
 class HomeScreen extends StatefulWidget {
   static const routeName = '/home-screen';
 
@@ -17,6 +16,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   var _taskController;
+  late List<Task> _tasks;
 
   void saveData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -24,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
     //prefs.setString('task', json.encode(t.getMap()) );
     //_taskController.text = '';
     String? tasks = prefs.getString('task');
-    List list = (tasks==null) ? [] : json.decode(tasks);
+    List list = (tasks == null) ? [] : json.decode(tasks);
     print(list);
     list.add(json.encode(t.getMap()));
     print(list);
@@ -33,11 +33,24 @@ class _HomeScreenState extends State<HomeScreen> {
     Navigator.of(context).pop();
   }
 
+  void _getTasks() async {
+    _tasks = [];
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? tasks = prefs.getString('task');
+    List list = (tasks == null) ? [] : json.decode(tasks);
+    for (dynamic d in list) {
+      _tasks.add(Task.fromMap(json.decode(d)));
+    }
+    print(_tasks);
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _taskController = TextEditingController();
+
+    _getTasks();
   }
 
   @override
@@ -45,19 +58,51 @@ class _HomeScreenState extends State<HomeScreen> {
     // TODO: implement dispose
     _taskController.dispose();
     super.dispose();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Task Manager',style: GoogleFonts.montserrat(),),
+        title: Text(
+          'Task Manager',
+          style: GoogleFonts.montserrat(),
+        ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('No Task Added Yet'),
-      ),
+      body: (_tasks == null)
+          ? Center(
+              child: Text('No Task Added Yet'),
+            )
+          : Column(
+              children: _tasks
+                  .map(
+                    (e) => Container(
+                      height: 70.0,
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 5.0),
+                      padding: const EdgeInsets.only(left: 10.0),
+                      alignment: Alignment.centerLeft,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 0.5,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(e.task,style: GoogleFonts.montserrat(),),
+                          Checkbox(value: false, onChanged: (value) {
+                            print('Tap');
+                          },)
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
       floatingActionButton: FloatingActionButton(
         child: Icon(
           Icons.add,
@@ -75,39 +120,44 @@ class _HomeScreenState extends State<HomeScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Add Task',style:GoogleFonts.montserrat(color: Colors.white,fontSize: 20),),
+                    Text(
+                      'Add Task',
+                      style: GoogleFonts.montserrat(
+                          color: Colors.white, fontSize: 20),
+                    ),
                     GestureDetector(
-                      onTap: ()=>Navigator.of(context).pop(),
+                      onTap: () => Navigator.of(context).pop(),
                       child: Icon(Icons.close),
                     ),
                   ],
-
                 ),
                 Divider(
                   thickness: 1.2,
                 ),
-                SizedBox(height: 20.0,),
+                SizedBox(
+                  height: 20.0,
+                ),
                 TextField(
                   controller: _taskController,
                   decoration: InputDecoration(
-                    border:OutlineInputBorder(
+                    border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(5.0),
                       borderSide: BorderSide(color: Colors.blue),
-
-                  ),
+                    ),
                     fillColor: Colors.white,
                     filled: true,
                     hintText: 'Enter Task',
                     hintStyle: GoogleFonts.montserrat(),
                   ),
                 ),
-                SizedBox(height: 20.0,),
+                SizedBox(
+                  height: 20.0,
+                ),
                 Container(
                   padding: const EdgeInsets.symmetric(horizontal: 5.0),
                   width: MediaQuery.of(context).size.width,
                   //height: 200.0,
                   child: Row(
-
                     children: [
                       Container(
                         width: (MediaQuery.of(context).size.width / 2) - 20,
@@ -117,22 +167,24 @@ class _HomeScreenState extends State<HomeScreen> {
                               onPrimary: Colors.white, // foreground
                             ),
                             onPressed: () => _taskController.text = '',
-                            child: Text('Reset',
-                              style: GoogleFonts.montserrat(color: Colors.black),)
-                        ),
+                            child: Text(
+                              'Reset',
+                              style:
+                                  GoogleFonts.montserrat(color: Colors.black),
+                            )),
                       ),
                       Container(
                         width: (MediaQuery.of(context).size.width / 2) - 20,
-
                         child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
                               primary: Colors.blue, // background
                               onPrimary: Colors.white, // foreground
                             ),
                             onPressed: () => saveData(),
-                            child: Text('Add',
-                              style: GoogleFonts.montserrat(),)
-                        ),
+                            child: Text(
+                              'Add',
+                              style: GoogleFonts.montserrat(),
+                            )),
                       ),
                     ],
                   ),
